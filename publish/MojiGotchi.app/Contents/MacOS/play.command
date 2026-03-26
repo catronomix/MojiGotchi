cd8d5103ae868b8ca537a3f3efdffb858acbe9cf
Incognite <42138122+catronomix@users.noreply.github.com>
2026-03-26 20:33:02 +0100

    Delete publish directory

--- publish/MojiGotchi.app/Contents/MacOS/play.command
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-HERE=$(dirname "$0")
-chmod +x "$HERE/MojiGotchi"
-"$HERE/MojiGotchi"
--- publish/linux_raw/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Get the directory where the script is located
-HERE=$(dirname "$(readlink -f "$0")")
-cd "$HERE"
-# Ensure the game has execution permissions
-chmod +x "./MojiGotchi"
-# Run the game
-"./MojiGotchi"
